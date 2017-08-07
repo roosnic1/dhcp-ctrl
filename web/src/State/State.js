0a95c8d9cc75a8a176b489628f85a46326f0358b
Nicolas Roos <nicolas.k.roos@gmail.com>
2017-08-07 20:45:56 +0200

    feat(web): added react for frontend

---
+++ web/src/State/State.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './State.css';
+
+class State extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dhcpEntries: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/v1/api/state')
+      .then(data => data.json())
+      .then((data) => {
+        this.setState({
+          dhcpEntries: data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="State">
+        <h1>State</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default State;
